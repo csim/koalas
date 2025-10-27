c6e5a1319d4416b0f5e57fbb029cdbd82ce21a6e
Clint Simon <clint.simon@microsoft.com>
2025-10-26 18:58:27 -0700

    save

--- src/Koalas/Text/TextTableBuilder.cs
+++ src/Koalas/Text/TextTableBuilder.cs
@@ -51,6 +51,13 @@ public interface ITextRowBuilder : IRender
 //_layoutBorderComputed = false;
 public class TextRowBuilder : ITextRowBuilder
 {
+    private readonly int _dataColumnCount;
+    private int _rowId = 1;
+    private readonly List<ITextRow> _rows;
+    private readonly TextTableBuilder _table;
+
+    public IReadOnlyList<ITextRow> Rows => _rows;
+
     public TextRowBuilder(
         TextTableBuilder table,
         List<ITextRow> rows,
@@ -63,13 +70,6 @@ public class TextRowBuilder : ITextRowBuilder
         List<TextColumn> textColumns = [.. columns.OfType<TextColumn>()];
         _dataColumnCount = textColumns.Count != 0 ? textColumns.Max(i => i.DataColumnIndex) + 1 : 0;
     }
-
-    private readonly int _dataColumnCount;
-    private int _rowId = 1;
-    private readonly List<ITextRow> _rows;
-    private readonly TextTableBuilder _table;
-
-    public IReadOnlyList<ITextRow> Rows => _rows;
 
     public ITextRowBuilder AddBorderRow()
     {
