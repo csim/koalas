30ec5df78bc252db4e6549889b595927db8d6e3b
Clint Simon <clint.simon@microsoft.com>
2026-01-06 08:45:30 -0800

    pypi version

--- src/python/outback/__init__.py
+++ src/python/outback/__init__.py
@@ -1,5 +1,8 @@
 """Koalas - Python utilities for .NET package dependency analysis and management."""
 
-__version__ = "0.1.0"
+try:
+    from .__version__ import __version__
+except ImportError:
+    __version__ = "0.0.0.dev0"
 
 __all__ = []
